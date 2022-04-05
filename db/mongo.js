7fedcc07ccd8b499aa6e3ed40f7198ed27eae0b6
ArcOrtiz <jaxortiz9@gmail.com>
2022-04-05 13:50:55 -0500

    Atlas

--- db/mongo.js
+++ db/mongo.js
@@ -3,7 +3,7 @@ const { MongoClient } = require('mongodb');
 // import { MongoClient } from 'mongodb'
 
 // Connection URL
-const url = 'mongodb://localhost:27017';
+const url = 'mongodb+srv://rogers:1234@cluster0.kmacb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const client = new MongoClient(url);
 
 // Database Name
