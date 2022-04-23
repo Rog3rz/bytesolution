c73ba30a22361809031826a9a05d10acdd468238
Dagoberto26 <dagiitho@hotmail.com>
2022-04-22 20:25:54 -0500

    puesto el formulario y la malloria del js, no muestra las cosas con estilo

---
+++ mercadopago.js
@@ -0,0 +1,104 @@
+var mercadopago = require('mercadopago');
+
+// Step #3
+const cardForm = mp.cardForm({
+    amount: "100.5",
+    autoMount: true,
+    form: {
+      id: "form-checkout",
+      cardholderName: {
+        id: "form-checkout__cardholderName",
+        placeholder: "Titular de la tarjeta",
+      },
+      cardholderEmail: {
+        id: "form-checkout__cardholderEmail",
+        placeholder: "E-mail",
+      },
+      cardNumber: {
+        id: "form-checkout__cardNumber",
+        placeholder: "Número de la tarjeta",
+      },
+      cardExpirationDate: {
+        id: "form-checkout__cardExpirationDate",
+        placeholder: "Fecha de vencimiento (MM/YYYY)",
+      },
+      securityCode: {
+        id: "form-checkout__securityCode",
+        placeholder: "Código de seguridad",
+      },
+      installments: {
+        id: "form-checkout__installments",
+        placeholder: "Cuotas",
+      },
+      identificationNumber: {
+        id: "form-checkout__identificationNumber",
+        placeholder: "Número de documento",
+      },
+      issuer: {
+        id: "form-checkout__issuer",
+        placeholder: "Banco emisor",
+      },
+    },
+    callbacks: {
+      onFormMounted: error => {
+        if (error) return console.warn("Form Mounted handling error: ", error);
+        console.log("Form mounted");
+      },
+      onSubmit: event => {
+        event.preventDefault();
+  
+        const {
+          paymentMethodId: payment_method_id,
+          issuerId: issuer_id,
+          cardholderEmail: email,
+          amount,
+          token,
+          installments,
+          identificationNumber
+        } = cardForm.getCardFormData();
+  
+        fetch("/process_payment", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            token,
+            issuer_id,
+            payment_method_id,
+            transaction_amount: Number(amount),
+            installments: Number(installments),
+            description: "Descripción del producto",
+            payer: {
+              email,
+              identification: {
+                number: identificationNumber,
+              },
+            },
+          }),
+        });
+      },
+      onFetching: (resource) => {
+        console.log("Fetching resource: ", resource);
+  
+        // Animate progress bar
+        const progressBar = document.querySelector(".progress-bar");
+        progressBar.removeAttribute("value");
+  
+        return () => {
+          progressBar.setAttribute("value", "0");
+        };
+      }
+    },
+  });
+  
+mercadopago.configurations.setAccessToken("APP_USR-1386608462969315-040520-be7e90c4357f3dd6164daa1c3b61471a-1101981994");
+
+mercadopago.payment.save(req.body)
+  .then(function(response) {
+    const { status, status_detail, id } = response.body;
+    res.status(response.status).json({ status, status_detail, id });
+  })
+  .catch(function(error) {
+    console.error(error);
+  });
